81544f0c0be27ee7607055990eb04e3c97f993c6
Ayush Raj <rajayush4102@gmail.com>
2023-05-14 17:50:26 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/Habit_Tracker_React/">
       <Provider store={store}>
         <App />
       </Provider>
