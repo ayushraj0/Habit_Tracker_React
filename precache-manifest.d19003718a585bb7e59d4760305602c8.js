f1bb2b5e240d3c6eb6e3c594c71c5ee910a7f3fa
Ayush Raj <rajayush4102@gmail.com>
2023-05-20 03:13:01 +0530

    Updates

---
+++ precache-manifest.d19003718a585bb7e59d4760305602c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a32d14f5bbad528da10",
+    "url": "/Habit_Tracker_React/static/css/main.90f543f8.chunk.css"
+  },
+  {
+    "revision": "3a32d14f5bbad528da10",
+    "url": "/Habit_Tracker_React/static/js/main.3a32d14f.chunk.js"
+  },
+  {
+    "revision": "f4b11dc3c579cb66c14c",
+    "url": "/Habit_Tracker_React/static/js/1.f4b11dc3.chunk.js"
+  },
+  {
+    "revision": "2fa7057cee9a62886f02",
+    "url": "/Habit_Tracker_React/static/js/runtime~main.2fa7057c.js"
+  },
+  {
+    "revision": "9214ce0ed2f9183e61a8b8f8ff714906",
+    "url": "/Habit_Tracker_React/static/media/habits.9214ce0e.jpg"
+  },
+  {
+    "revision": "d33ac1b752eb4380e17c577de68011e2",
+    "url": "/Habit_Tracker_React/index.html"
+  }
+];
